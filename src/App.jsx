d5f9b5eb3c8134e41d5dec743e036798e44584c4
Master-mohit <mohitmansigupta9098@gmail.com>
2024-12-19 00:42:17 +0530

    useeffect implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import axios from 'axios'
-import React from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
-  const [products, setproducts] = useState([])
+  const [count, setCount] = useState(0);
+  const counhandler = () => {
+    setCount(count +1 )
+  }
 
   useEffect(() => {
-   const fetch = async() => {
-    const response = await axios.get('https://fakestoreapi.com/products')
-    setproducts(response.data)
-   }
-   fetch();
-  }, [])
-  
-  return (
-    <div className=' w-screen h-fit p-10 text-black font-semibold'>
-     {products.map((pro) => (
-          <div className='flex flex-col justify-evenly gap-10 w-full h-full'>
-          <div className='bg-zinc-300 w-[17vw] h-[27vh] object-contain overflow-hidden' >
-            <img src={pro.image} alt=''/>
-          </div>
-          <div className='text-[20px] mb-4'>
-            <h1>Title:- {pro.title}</h1>
-            <h1>Price:- {pro.price}</h1>
-            <h1>Description:- {pro.description}</h1>
-          </div>
-          </div>
-     ))}
-    </div>
-  )
-}
+    console.log(`Count is now: ${count}`);
+  }, [count]);
 
-export default App
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={counhandler}>Increment</button>
+    </div>
+  );
+};
+
+export default App;
